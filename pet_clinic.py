73639892c44ac7c7af9f2b78251984fe0a414221
unknown <ncespedes.art@gmail.com>
2025-08-07 15:27:46 -0400

    fixed appointments

--- pet_clinic.py
+++ pet_clinic.py
@@ -67,7 +67,7 @@ Base.metadata.create_all(bind=engine)
 #============== Adding Data =================
 
 
-#Creating a 3 new_owners and adding them to the db
+#Creating at least 3 new_owners and adding them to the db
 new_owners = [
     Owners(name = "Shaggy Rogers", phone = "555-123-1969", email= "norvillerogers@mysteryinc.com"),
     Owners(name = "Charlie Brown", phone = "555-111-1950", email= "peanuts@email.com"),
@@ -75,43 +75,51 @@ new_owners = [
     Owners(name = "Harry Potter", phone = "555-777-1997", email= "yerawizard@email.com"),
     Owners(name = "John Arbuckle", phone = "555-222-1978", email= "lasagna@email.com")
 ]
-session.add(new_owners)
-session.commit()
+# session.add_all(new_owners)
+# session.commit()
 
 
-# new_pets = [
-#     Pets(name = "Snoopy", species = "Dog", breed= "Beagle", age=5, owner_id=2),
-#     Pets(name = "Scooby-Doo", species = "Dog", breed= "Great Dane", age=9, owner_id=1),
-#     Pets(name = "Hedwig", species = "Owl", breed= "Snowy Owl", age=7, owner_id=4),
-#     Pets(name = "Garfield", species = "Cat", breed= "Domestic Shorthair", age=10, owner_id=5),
-#     Pets(name = "Odie", species = "Dog", breed= "Mixed", age=4, owner_id=5),
-#     Pets(name = "Duchess", species = "Cat", breed= "Turkish Angora", age=9, owner_id=3),
-#     Pets(name = "Marie", species = "Cat", breed= "Turkish Angora", age=0, owner_id=3),
-#     Pets(name = "Berlioz", species = "Cat", breed= "Turkish Angora", age=0, owner_id=3),
-#     Pets(name = "Toulouse", species = "Cat", breed= "Turkish Angora", age=0, owner_id=3),
-#     Pets(name = "Thomas O'Malley", species = "Cat", breed= "Domestic Shorthair", age=7, owner_id=3)
-# ]
+new_pets = [
+    Pets(name = "Snoopy", species = "Dog", breed= "Beagle", age=5, owner_id=2),
+    Pets(name = "Scooby-Doo", species = "Dog", breed= "Great Dane", age=9, owner_id=1),
+    Pets(name = "Hedwig", species = "Owl", breed= "Snowy Owl", age=7, owner_id=4),
+    Pets(name = "Garfield", species = "Cat", breed= "Domestic Shorthair", age=10, owner_id=5),
+    Pets(name = "Odie", species = "Dog", breed= "Mixed", age=4, owner_id=5),
+    Pets(name = "Duchess", species = "Cat", breed= "Turkish Angora", age=9, owner_id=3),
+    Pets(name = "Marie", species = "Cat", breed= "Turkish Angora", age=0, owner_id=3),
+    Pets(name = "Berlioz", species = "Cat", breed= "Turkish Angora", age=0, owner_id=3),
+    Pets(name = "Toulouse", species = "Cat", breed= "Turkish Angora", age=0, owner_id=3),
+    Pets(name = "Thomas O'Malley", species = "Cat", breed= "Domestic Shorthair", age=7, owner_id=3)
+]
 
-# session.add(new_pets)
+# session.add_all(new_pets)
 # session.commit()
 
 
 #Creating a 2 new_vets and adding them to the db
-# new_vets = [
-#     Veterinarians(name = "John Dolittle", specialization = "General", email= "talkinganimals@email.com"),
-#     Veterinarians(name = "James Herriot", specialization = "Surgery", email= "allpetsbigandsmall@email.com")
-# ]
-# session.add(new_vets)
+new_vets = [
+    Veterinarians(name = "John Dolittle", specialization = "General", email= "talkinganimals@email.com"),
+    Veterinarians(name = "James Herriot", specialization = "Surgery", email= "allpetsbigandsmall@email.com")
+]
+
+# session.add_all(new_vets)
 # session.commit()
 
-# new_appts = [
-#     Appointments(pet_id = 0, veterinarian_id = 0, appointment_date = (2025, 1, 15, 10, 30, 0), notes = "", status = "Scheduled"),
-#     Appointments(pet_id = 0, veterinarian_id = 0, appointment_date = (2025, 1, 15, 10, 30, 0), notes = "", status = "Scheduled")
-# ]
-# session.add(new_appts)
+new_appts = [
+    Appointments(pet_id = 6, veterinarian_id = 1, appointment_date = datetime(2025, 8, 15, 10, 30, 0), notes = "Well behaved, model cat. No previous problems.", status = "Scheduled"),
+    Appointments(pet_id = 7, veterinarian_id = 1, appointment_date = datetime(2025, 8, 15, 10, 45, 0), notes = "Checkup, no known problems. Warning: Do not mess up her bow.", status = "Scheduled"),
+    Appointments(pet_id = 8, veterinarian_id = 1, appointment_date = datetime(2025, 8, 15, 11, 15, 0), notes = "To avoid distractions, keep away from his brother. Likes to bite", status = "Scheduled"),
+    Appointments(pet_id = 9, veterinarian_id = 1, appointment_date = datetime(2025, 8, 15, 11, 30, 0), notes = "To avoid distractions, keep away from his brother. Likes to bite", status = "Scheduled"),
+    Appointments(pet_id = 10, veterinarian_id = 1, appointment_date = datetime(2025, 8, 15, 11, 45, 0), notes = "Adopted alleyway cat. Needs thorough checkup. Seems to be unfamiliar with vets.", status = "Scheduled"),
+    Appointments(pet_id = 1, veterinarian_id = 1, appointment_date = datetime(2025, 9, 15, 10, 45, 0), notes = "Address the dog directly, not the owner", status = "Scheduled"),
+    Appointments(pet_id = 2, veterinarian_id = 2, appointment_date = datetime(2025, 9, 25, 11, 30, 0), notes = "Clear the office, he will eat everything", status = "Cancelled"),
+    Appointments(pet_id = 4, veterinarian_id = 2, appointment_date = datetime(2025, 7, 30, 10, 30, 0), notes = "Low motivation to exercise. Has history of eating too much lasagna. Check blood report.", status = "Completed"),
+    Appointments(pet_id = 5, veterinarian_id = 2, appointment_date = datetime(2025, 7, 30, 11, 30, 0), notes = "Struggles to keep tongue in mouth. Check for dryness. Very sweet dog.", status = "Completed"),
+    Appointments(pet_id = 3, veterinarian_id = 1, appointment_date = datetime(2025, 7, 15, 10, 30, 0), notes = "Well behaved. Has shown wing fatigue before.", status = "Completed")
+]
+# session.add_all(new_appts)
 # session.commit()
 
-#Scheduled, Completed, Cancelled
-
+# print("hello testing")
 
 
